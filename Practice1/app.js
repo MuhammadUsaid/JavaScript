ae39e1e76ef85528a6acf92f0ccedc7569f8684d
MuhammadUsaid <mohammadusaid40@gmail.com>
2016-09-11 05:54:37 -0700

    first practice of javascript

---
+++ Practice1/app.js
@@ -0,0 +1,7 @@
+var name = prompt("What is your name?");
+var greet = "Hello " + name + " Welcome To MyPage";
+alert(greet);
+var number1 = +prompt("Enter a Number Of your Choice");
+var number2 = +prompt("Please choose another number");
+alert("Here is the product of your numbers " + (number1 * number2) );
+alert("Here is the sum of your numbers " + (number1 + number2));
