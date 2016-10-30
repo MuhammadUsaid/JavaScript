f0f32fbb1280131b317a8f54339f966ed4fdd336
MuhammadUsaid <mohammadusaid40@gmail.com>
2016-10-30 08:34:12 -0700

    table assignment updated

--- 4/app.js
+++ 4/app.js
@@ -1,8 +1,8 @@
 var num = +prompt("Enter A Number");
-for (var i = 0; i <= 10; i++) {
+for (var i = 0; i <= 20; i++) {
     document.getElementById("number"+ i).innerHTML = num;
 }
 
-for (var i = 0; i <= 10; i++) {
+for (var i = 0; i <= 20; i++) {
     document.getElementById("answer"+i).innerHTML = i * num;
 }
